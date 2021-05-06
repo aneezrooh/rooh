a5f01768c5b08550a67b35de77051701fd29ba66
Sreekumar <73136964+sakhaavvaavaj93@users.noreply.github.com>
2021-05-06 21:59:54 +0530

    Update userbotjoin.py

--- handlers/userbotjoin.py
+++ handlers/userbotjoin.py
@@ -21,7 +21,7 @@ async def addchannel(client, message):
         user = await USER.get_me()
 
         user.first_name =  "DaisyMusix"
-     except:
+
         await USER.join_chat(invitelink)
         await USER.send_message(message.chat.id,"I joined here as you requested")
     except UserAlreadyParticipant:
